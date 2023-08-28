298a1941aad2c5473c03d2b71df1bd6fc98a5509
Paliy Stepan <palstepweb@gmail.com>
2023-08-28 10:37:13 +0300

    wip

---
+++ frontend/src/layouts/SimpleLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot />
+</template>
+
+<script>
+export default {
+  name: "SimpleLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/CartView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="showYes">yes</div>
+  <span v-else>no</span>
+</template>
+
+<script>
+import { h, ref } from 'vue';
+
+const showYes = ref(true)
+
+export default {
+  setup() {
+    return () => h('div', [showYes.value ? h('div', 'yes') : h('span', 'no')])
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
